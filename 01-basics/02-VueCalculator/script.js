b084a5434d56285944dc58830d8273d37c09a297
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-01 18:39:16 +0200

    feat(01-basics) added VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,31 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Root = defineComponent({
+  name: 'Root',
+
+  data() {
+    return {
+      number1: 0,
+      number2: 0,
+      mathOperation: '',
+    };
+  },
+  computed: {
+    operationResult() {
+      switch (this.mathOperation) {
+        case 'sum':
+          return this.number1 + this.number2;
+        case 'subtract':
+          return this.number1 - this.number2;
+        case 'multiply':
+          return this.number1 * this.number2;
+        case 'divide':
+          return this.number1 / this.number2;
+        default:
+          return '';
+      }
+    },
+  },
+});
+
+createApp(Root).mount('#app');
