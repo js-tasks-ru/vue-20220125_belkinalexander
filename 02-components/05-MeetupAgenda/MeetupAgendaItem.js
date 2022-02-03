fbf9c79bc2156e1d1d912a25d106eb411a73b7a3
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-03 22:25:05 +0200

    fix(02-components) added 05-MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgendaItem.js
+++ 02-components/05-MeetupAgenda/MeetupAgendaItem.js
@@ -1,23 +1,42 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
+import { agendaItemIcons, agendaItemDefaultTitles } from './meetupService.js';
 
 export default defineComponent({
   name: 'MeetupAgendaItem',
 
+  props: {
+    agendaItem: {
+      type: Object,
+      require: true,
+    },
+  },
+
+  computed: {
+    agendaItemIcon() {
+      return `/assets/icons/icon-${agendaItemIcons[this.agendaItem.type]}.svg`;
+    },
+    agendaItemTitle() {
+      return this.agendaItem.title || agendaItemDefaultTitles[this.agendaItem.type];
+    },
+    agendaItemTime() {
+      return `${this.agendaItem.startsAt} - ${this.agendaItem.endsAt}`;
+    },
+  },
+
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img src="/assets/icons/icon-key.svg" class="icon" alt="key" />
+        <img :src="agendaItemIcon" class="icon" alt="key" />
       </div>
-      <div class="agenda-item__col">00:00 - 00:00</div>
+      <div class="agenda-item__col">{{ agendaItemTime }}</div>
       <div class="agenda-item__col">
-        <h3 class="agenda-item__title">Title</h3>
-        <p class="agenda-item__talk">
-          <span>Talk Speaker</span>
+        <h3 class="agenda-item__title">{{ agendaItemTitle }}</h3>
+        <p class="agenda-item__talk" v-if="this.agendaItem.type === 'talk'">
+          <span>{{ this.agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
-          <span class="agenda-item__lang">EN</span>
+          <span class="agenda-item__lang">{{ this.agendaItem.language }}</span>
         </p>
-        <p>Description</p>
+        <p>{{ this.agendaItem.description || '' }}</p>
       </div>
     </div>`,
 });
