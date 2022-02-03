a22a7fc32157d5da99edd083c511301dd6dd1257
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-03 18:50:48 +0200

    fix(02-components) added 03-MeetupCover

--- 02-components/03-MeetupCover/MeetupCover.js
+++ 02-components/03-MeetupCover/MeetupCover.js
@@ -3,8 +3,19 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'MeetupCover',
 
+  props: {
+    title: String,
+    image: String,
+  },
+
+  computed: {
+    meetupImage() {
+      return this.image ? { '--bg-url': `url(${this.image})` } : '';
+    },
+  },
+
   template: `
-    <div class="meetup-cover" style="--bg-url: url('https://course-vue.javascript.ru/api/images/2')">
-        <h1 class="meetup-cover__title">Title</h1>
+    <div class="meetup-cover" :style="meetupImage">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
     </div>`,
 });
