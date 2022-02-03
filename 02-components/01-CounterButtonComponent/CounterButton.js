f403bfde4ac7dcad77422e63ef9490210f739cc1
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-03 17:39:52 +0200

    fix(02-components) added 01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -3,10 +3,28 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 export default defineComponent({
   name: 'CounterButton',
 
-  // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
+  emits: {
+    'update:count': null,
+  },
 
-  // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  data() {
+    return {
+      counter: 1,
+    };
+  },
+
+  methods: {
+    buttonOnClick(value) {
+      this.$emit('update:count', this.count + value);
+    },
+  },
+
+  template: `<button type="button" @click="buttonOnClick(counter)">{{ count }}</button>`,
 });
