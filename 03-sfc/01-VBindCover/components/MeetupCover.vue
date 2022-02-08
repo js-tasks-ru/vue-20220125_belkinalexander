c02472b6550f2a5bc2e4aba3f8f7dc64da022d3b
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-08 17:37:49 +0200

    resolve(03-sfc) 01-VBindCover

--- 03-sfc/01-VBindCover/components/MeetupCover.vue
+++ 03-sfc/01-VBindCover/components/MeetupCover.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="meetup-cover">
-    <h1 class="meetup-cover__title">Title</h1>
+    <h1 class="meetup-cover__title">{{ title }}</h1>
   </div>
 </template>
 
 <script>
+const BACKGROUND_IMAGE_BASE_STYLE = 'linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),';
+
 export default {
   name: 'MeetupCover',
+
+  props: {
+    title: String,
+    image: String,
+  },
+
+  computed: {
+    backgroundImage() {
+      return this.image
+        ? `${BACKGROUND_IMAGE_BASE_STYLE} url(${this.image})`
+        : `${BACKGROUND_IMAGE_BASE_STYLE} var(--default-cover)`;
+    },
+  },
 };
 </script>
 
@@ -14,9 +29,7 @@ export default {
 .meetup-cover {
   background-size: cover;
   background-position: center;
-  background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),
-    url('https://course-vue.javascript.ru/api/images/2');
-  /* background-image: linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), var(--default-cover); */
+  background-image: v-bind(backgroundImage);
   display: flex;
   flex-direction: column;
   align-items: center;
