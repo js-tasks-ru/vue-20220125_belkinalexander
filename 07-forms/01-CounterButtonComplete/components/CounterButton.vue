6b546dbc30417d9d03db746d80ce097d9a23e38e
Alexander Belkin <belkin.alexander@gmail.com>
2022-02-22 11:05:52 +0200

    resolve(07-forms) 01-CounterButtonComplete

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,38 @@
 <template>
-  <button></button>
+  <button @click="onClick">{{ localCounter }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  emits: ['update:count'],
+
+  data() {
+    return {
+      localCounter: this.count,
+    };
+  },
+
+  watch: {
+    count: {
+      handler(value) {
+        this.localCounter = value;
+      },
+    },
+  },
+
+  methods: {
+    onClick() {
+      this.$emit('update:count', (this.localCounter += 1));
+    },
+  },
 };
 </script>
